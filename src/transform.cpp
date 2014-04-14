80ecf1a2e6067a0b0dfc7d5b78dc6d7b34e3047f
David Greaves <david@dgreaves.com>
2014-04-14 23:08:36 +0100

    Simple (correct?) way to handle this multiplication
    Signed-off-by: David Greaves <david@dgreaves.com>

--- src/transform.cpp
+++ src/transform.cpp
@@ -12,7 +12,9 @@ Transform::Transform(QMatrix4x4 t) :
 void Transform::rotateOnly(QMatrix4x4 r)
 {
     QVector4D p = position(); // store position
-    // this = r * this; // rotate
-    QMatrix4x4::operator*=(r); // rotate
+    *this = r * *this; // rotate
+//    QMatrix4x4 tmp(r * *this);
+//    tmp.copyDataTo(this->data());
+//    this->optimize();
     setPosition(p); // restore position
 }
