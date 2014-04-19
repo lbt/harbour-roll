a34fc44f32aa476b9f4f26fd165bc871db34ca5a
David Greaves <david@dgreaves.com>
2014-04-19 23:11:42 +0100

    Make WorldItem fully virtual
    Signed-off-by: David Greaves <david@dgreaves.com>

--- src/worlditem.h
+++ src/worlditem.h
@@ -18,33 +18,34 @@ class WorldItem : public QObject
 public:
     explicit WorldItem(QString name);
 
-    void addRenderable(Renderable* r);
-    void setMotionManager(MotionManager *m);
-    MotionManager* motion() const { return m_motion;}
-    Physics* physics() { return dynamic_cast<Physics*>(m_motion);}
+    virtual void addRenderable(Renderable* r);
+    virtual void setMotionManager(MotionManager *m);
+    virtual MotionManager* motion() const { return m_motion;}
+    virtual Physics* physics() { return dynamic_cast<Physics*>(m_motion);}
 
-    void setupGL();
-    void render(const Shader *activeProgram);
+    virtual void setupGL();
+    virtual void render(const Shader *activeProgram);
 
-    void setVelocity(QVector3D v) { m_motion->setVelocity(v); }
-    QVector3D getVelocity() const { return m_motion->getVelocity(); }
-    void setTransform(QMatrix4x4 t) { m_motion->setTransform(t); }
-    Transform getTransform() const { return m_motion->getTransform(); }
+    virtual void setVelocity(QVector3D v) { m_motion->setVelocity(v); }
+    virtual QVector3D getVelocity() const { return m_motion->getVelocity(); }
+    virtual void setTransform(QMatrix4x4 t) { m_motion->setTransform(t); }
+    virtual Transform getTransform() const { return m_motion->getTransform(); }
 
-    void addToWorld(World* world);
-    void removeFromWorld();
+    virtual void addToWorld(World* world);
+    virtual void removeFromWorld();
 
 signals:
 
 public slots:
 
 protected:
-    bool inWorld(); // If in a World
-    QList<Shader *> shaderList();
+    virtual bool inWorld(); // If in a World
+    virtual QList<Shader *> shaderList();
 
 private:
     QList<Renderable*> m_renderables;
     MotionManager* m_motion;
+    QString m_name;
 };
 
 #endif // WORLDITEM_H
