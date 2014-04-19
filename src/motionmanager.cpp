fdfbcced64cb08d90cc8b1561b4b0aae05578780
David Greaves <david@dgreaves.com>
2014-04-19 17:53:46 +0100

    First cut at a MotionManager
    Signed-off-by: David Greaves <david@dgreaves.com>

---
+++ src/motionmanager.cpp
@@ -0,0 +1,17 @@
+#include "motionmanager.h"
+#include "worlditem.h"
+
+MotionManager::MotionManager(WorldItem *parent) :
+    QObject(parent)
+{
+}
+
+void MotionManager::setTransformVelocity(Transform t, QVector3D v)
+{
+    m_transform = t;
+    m_velocity = v;
+}
+
+void MotionManager::runStep(int deltaTms) {
+    m_transform.translate(m_velocity * (deltaTms/1000.0));
+}
---
+++ src/motionmanager.h
@@ -0,0 +1,31 @@
+#ifndef MOTIONMANAGER_H
+#define MOTIONMANAGER_H
+
+#include <QObject>
+#include "transform.h"
+
+class WorldItem;
+
+class MotionManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MotionManager(WorldItem* parent = 0);
+    virtual void setTransformVelocity(Transform t, QVector3D v=QVector3D(0,0,0));
+    virtual void setTransform(Transform t) {  m_transform = t; }
+    virtual Transform getTransform() const { return m_transform; }
+    virtual void setVelocity(QVector3D v) { m_velocity = v; }
+    virtual QVector3D getVelocity() const { return m_velocity; }
+
+    virtual void runStep(int deltaTms);
+
+signals:
+
+public slots:
+
+protected:
+    Transform m_transform;
+    QVector3D m_velocity;
+};
+
+#endif // MOTIONMANAGER_H
