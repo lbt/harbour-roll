3669ca30ea0b671d204af4f0a05b9ced3c2c866e
David Greaves <david@dgreaves.com>
2014-04-21 21:52:22 +0100

    Renderables can be marked as Translucent
    Signed-off-by: David Greaves <david@dgreaves.com>

--- src/renderable.cpp
+++ src/renderable.cpp
@@ -16,6 +16,8 @@ Renderable::Renderable(QString name, VAO *v, QObject *parent) :
     QObject(parent)
   , m_shader(NULL)
   , m_vao(v)
+  , m_opaque(true)
+
 {
     setObjectName(name);
     if (!m_vao) {
--- src/renderable.h
+++ src/renderable.h
@@ -14,6 +14,10 @@ public:
     explicit Renderable(QString name, VAO* v, QObject *parent = 0);
     virtual void setShader(Shader* p) { m_shader = p; }
     virtual Shader* getShader() const { return m_shader; }
+    virtual void setTranslucent(bool translucent) { m_opaque = ! translucent; }
+    virtual bool isTranslucent() { return !m_opaque ; }
+    virtual bool isOpaque() const { return m_opaque ; }
+
     virtual void setupGL() = 0;
     virtual void render(const Shader *activeShader, QMatrix4x4 worldMatrix) = 0;
 
@@ -25,6 +29,7 @@ protected:
     Shader* m_shader;
     VAO *m_vao;
     GLuint m_vboIds[2];
+    bool m_opaque;
 };
 
 #endif // RENDERABLE_H
